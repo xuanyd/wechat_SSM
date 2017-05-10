54dd2a6c74fad80cb8eeed129cf9f3a2a1cebca0
xuanyd <xuanyd@aliyun.com>
2017-05-10 15:45:06 +0800

    update static html

--- src/main/java/com/wechat/controller/GreetingController.java
+++ src/main/java/com/wechat/controller/GreetingController.java
@@ -17,7 +17,7 @@ public class GreetingController {
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        User user = userService.selectUserById(5);
+        User user = userService.selectUserById(new Integer(5));
         System.out.println("--------" + user);
         return "listuser";
     }
